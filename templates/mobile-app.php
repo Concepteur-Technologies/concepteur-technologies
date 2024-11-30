4abcf4e05311db05d08c2f9f1e8b74299e10ec84
Proparna Das <proparna.das.cc@gmail.com>
2024-11-30 20:04:42 +0530

    SDSDS

---
+++ templates/mobile-app.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Mobile App
+*/
+get_header(); ?>
+
+
+<section class=" page-title-big-typography cover-background" style="background-color: white">
+      <div class="opacity-very-light main-bnr-image"></div>
+      <div class="container">
+            <div class="row extra-very-small-screen align-items-center justify-content-center">
+                  <div class="col-lg-5 col-sm-8 position-relative page-title-extra-small appear anime-child anime-complete" data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;opacity&quot;: [0, 1], &quot;translateX&quot;: [-30, 0], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                        <div class="cd-headline slide">
+                              <div class="fs-120 lg-fs-100 text-dark-gray lh-100 fw-500 mb-10 alt-font ls-minus-5px fancy-text-style-4" data-anime='{ "el": "childs", "opacity": [0, 1], "translateX": [-100, 0], "staggervalue": 300, "duration": 1000, "easing": "easeOutCubic" }'>
+                                    <span class="fw-700 d-inline-block" data-fancy-text='{ "effect": "wave", "string": ["Android", "IOS"], "duration": 4000 }' data-bottom-top="transform: translateX(-70px)" data-top-bottom="transform: translateX(70px)"></span>
+                                    <span class="d-inline-block fs-80" data-bottom-top="transform: translateX(70px)" data-top-bottom="transform: translateX(-70px)">Development</span>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-lg-5 col-sm-8 position-relative page-title-extra-small appear anime-child anime-complete" data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;opacity&quot;: [0, 1], &quot;translateX&quot;: [-30, 0], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                        <h1 class="mb-20px xs-mb-20px text-black text-shadow-medium"><span class="w-30px h-2px bg-yellow d-inline-block align-middle position-relative top-minus-2px me-10px"></span>Popular services</h1>
+                        <h2 class="text-black text-shadow-medium fw-500 ls-minus-2px mb-0">Professional services for <br></h2>
+                  </div>
+            </div>
+      </div>
+</section>
+
+<?php get_footer(); ?>
