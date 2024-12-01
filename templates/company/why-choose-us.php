bf947aec634f69aa73338cf973799a6ec9a84fd9
Proparna Das <proparna.das.cc@gmail.com>
2024-12-01 16:22:09 +0530

    ds

---
+++ templates/company/why-choose-us.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Template Name: Why Choose Us
+ */
+get_header();
+?>
+
+<section class="cover-background bg-transparent ipad-top-space-margin position-relative section-dark md-h-auto">
+      <div class=" main-bnr-image-two opacity-all"></div>
+      <div class="container">
+            <div class="row align-items-center justify-content-center small-screen">
+                  <div class="col-xl-6 col-lg-7 col-sm-8 position-relative text-center page-title-extra-small" data-anime='{ "el": "childs", "translateY": [30, 0], "opacity": [0,1], "duration": 400, "delay": 0, "staggervalue": 100, "easing": "easeOutQuad" }'>
+                        <div>
+                              <h1 class="text-uppercase mb-15px alt-font text-white opacity-6 fw-500 ls-2px" data-fancy-text='{ "opacity": [0, 1], "translateY": [50, 0], "filter": ["blur(20px)", "blur(0px)"], "string": ["About our business"], "duration": 400, "delay": 0, "speed": 50, "easing": "easeOutQuad" }'></h1>
+                        </div>
+                        <h2 class="m-auto text-white alt-font text-shadow-double-large fw-700 w-90 xl-w-100" data-fancy-text='{ "opacity": [0, 1], "translateY": [50, 0], "filter": ["blur(20px)", "blur(0px)"], "string": ["We are specialize in startups"], "duration": 400, "delay": 0, "speed": 50, "easing": "easeOutQuad" }'></h2>
+                  </div>
+                  <div class="down-section text-center" data-anime='{ "translateY": [-50, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
+                        <a href="#down-section" class="section-link">
+                              <div class="text-white">
+                                    <i class="line-icon-Down icon-medium"></i>
+                              </div>
+                        </a>
+                  </div>
+            </div>
+      </div>
+</section>
+<?php include_once(get_template_directory() . '/global/contact-us-section.php'); ?>
+
+<?php get_footer() ?>
+<?php get_footer() ?>
