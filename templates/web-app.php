266bc0ce415ab60fd81f2914b3e59cfb529e309b
Proparna Das <proparna.das.cc@gmail.com>
2024-12-26 05:02:08 +0530

    new wors

--- templates/web-app.php
+++ templates/web-app.php
@@ -4,8 +4,7 @@ Template Name: web-app
 */
 get_header(); ?>
 
-
-<section class=" page-title-big-typography cover-background" style="background-color: white; padding-bottom: 304px">
+<section class="page-title-big-typography cover-background" style="background-color: white; padding-bottom: 304px">
       <div class="opacity-very-light main-bnr-image"></div>
       <div class="container">
             <div class="row extra-very-small-screen align-items-center justify-content-center">
@@ -17,28 +16,23 @@ get_header(); ?>
                               </div>
                         </div>
                         <div class="col-auto col-xxl-5 col-lg-7 col-sm-9 d-flex align-items-center justify-content-center" style="width: 100% !important;">
-                              <span class="fs-150 text-majorelle-blue flex-shrink-0 fw-800 me-35px ls-minus-10px sm-ls-minus-5px skrollable skrollable-between" data-bottom-top="transform:scale(1.3, 1.3);" data-top-bottom="transform:scale(1, 1);" style="transform: scale(1.21926, 1.21926);">13+</span>
+                              <span class="fs-90 text-majorelle-blue flex-shrink-0 fw-800 me-35px ls-minus-10px sm-ls-minus-5px skrollable skrollable-between" data-bottom-top="transform:scale(1.3, 1.3);" data-top-bottom="transform:scale(1, 1);" style="transform: scale(1.21926, 1.21926);">50+</span>
                               <h2 class="text-dark-gray fw-700 ls-minus-2px m-0">Projects Delivered.</h2>
                         </div>
                   </div>
 
                   <div class="col-lg-5 col-sm-8 position-relative page-title-extra-small appear anime-child anime-complete" data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;opacity&quot;: [0, 1], &quot;translateX&quot;: [-30, 0], &quot;duration&quot;: 800, &quot;delay&quot;: 0, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
-                        <h1 class="mb-20px xs-mb-20px text-black text-shadow-medium"><span class="w-30px h-2px bg-yellow d-inline-block align-middle position-relative top-minus-2px me-10px"></span>Mobile App Services</h1>
+                        <h1 class="mb-20px xs-mb-20px text-black text-shadow-medium"><span class="w-30px h-2px bg-yellow d-inline-block align-middle position-relative top-minus-2px me-10px"></span>Web Development Services</h1>
                         <div class="cart-box">
                               <ul class="cart-items">
                                     <li class="cart-item">
-                                          <h4 class="item-title">iOS App </h4>
-                                          <p class="item-description">Custom mobile apps tailored for iOS devices with seamless performance.</p>
+                                          <h4 class="item-title">Custom Web & E-commerce Development</h4>
+                                          <p class="item-description">We specialize in building responsive, high-performance websites and custom e-commerce platforms. From seamless user experiences across devices to advanced features like payment integration and product management, we tailor solutions to meet your business needs.</p>
                                     </li>
                                     <li class="cart-item">
-                                          <h4 class="item-title">Android App </h4>
-                                          <p class="item-description">Building intuitive and high-performance apps for Android users.</p>
+                                          <h4 class="item-title">Content Management Systems (CMS)</h4>
+                                          <p class="item-description">Empowering your team with easy-to-use content management systems for seamless website updates and scalability.</p>
                                     </li>
-                                    <li class="cart-item">
-                                          <h4 class="item-title">Cross-platform </h4>
-                                          <p class="item-description">Reach both iOS and Android with a unified solution.</p>
-                                    </li>
-                                    <a href="#" class="btn btn-link-gradient btn-medium text-gradient-fast-blue-purple d-table d-lg-inline-block xl-mb-15px md-mx-auto">Get Started<span class="btn-icon"><i class="fa-solid fa-arrow-right"></i></span><span class="bg-gradient-fast-blue-purple"></span></a>
                               </ul>
                         </div>
                   </div>
@@ -522,6 +516,173 @@ get_header(); ?>
             </div>
       </div>
 </section>
+
+
+<section>
+      <div class="container">
+            <div class="row justify-content-center mb-3">
+                  <div class="col-xl-7 col-lg-6 col-md-7 text-center" data-anime='{ "el": "childs", "translateY": [30, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
+                        <span class="ps-25px pe-25px mb-20px text-uppercase text-base-color fs-14 lh-34 fw-600 border-radius-100px bg-solitude-blue d-inline-block">Flexible Hiring</span>
+                        <h2 class="text-dark-gray alt-font fw-700 ls-minus-1px">Find the Perfect Hiring Plan for Your Web Application</h2>
+                  </div>
+            </div>
+            <div class="row align-items-end pricing-table-style-05 g-0 mb-5 background-position-center background-no-repeat justify-content-center" style="background-image: url('https://res.cloudinary.com/didxyeaj1/image/upload/v1735165891/demo-corporate-bg-03_jtxrrj.png')">
+                  <div class="col-lg-4 col-md-8 md-mb-30px" data-anime='{ "translateX": [50, 0], "opacity": [0,1], "duration": 1200, "delay": 0, "staggervalue": 150, "easing": "easeOutQuad" }'>
+                        <div class="border-radius-6px position-relative overflow-hidden">
+                              <div class="pricing-header pt-45px pb-10px border-radius-6px text-center position-relative top-minus-3px">
+                                    <span class="ps-25px pe-25px mb-15px text-uppercase text-base-color fs-13 lh-34 fw-700 border-radius-100px bg-solitude-blue d-inline-block">Starter</span>
+                                    <h5 class="fw-700 mb-0 text-dark-gray alt-font">Fixed-Cost Hiring</h5>
+                                    <div class="pricing-body pt-35px">
+                                          <ul class="p-0 m-0 list-style-02 fw-500">
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10" style="text-align: left;">
+                                                      This plan offers a fixed cost based on an initial analysis of your project requirements.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10" style="text-align: left;">
+                                                      The project scope and cost are defined upfront, with any changes incurring additional fees.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10" style="text-align: left;">
+                                                      Ideal for clients with a clear, well-defined scope and a set vision for their project.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10" style="text-align: left;">
+                                                      Fixed-cost projects start at $2000 and can go up depending on project complexity and analytics.
+                                                </li>
+                                          </ul>
+                                    </div>
+                              </div>
+                              <div class="pricing-footer ps-12 pe-12 pb-8 text-center">
+                                    <a href="#" class="btn btn-large btn-dark-gray btn-box-shadow btn-hover-animation-switch btn-round-edge w-100 text-transform-none mb-15px">
+                                          <span>
+                                                <span class="btn-text">Start Your Project </span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                          </span>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-lg-4 col-md-8 md-mb-30px" data-anime='{ "translateX": [0, 0], "opacity": [0,1], "duration": 1200, "delay": 0, "staggervalue": 150, "easing": "easeOutQuad" }'>
+                        <div class="bg-dark-gray border-radius-6px overflow-hidden position-relative box-shadow-double-large z-index-9">
+                              <div class="p-10px fw-700 fs-14 text-white bg-base-color text-uppercase text-center">Popular</div>
+                              <div
+                                    class="pricing-header pt-45px pb-10px bg-white border-radius-6px text-center position-relative top-minus-3px">
+                                    <span class="ps-25px pe-25px mb-15px text-uppercase text-base-color fs-13 lh-34 fw-700 border-radius-100px bg-solitude-blue d-inline-block">Professional</span>
+                                    <h5 class="fw-700 mb-0 text-dark-gray alt-font">Full-Time Hiring</h5>
+                                    <div class="pricing-body pt-35px">
+                                          <ul class="p-0 m-0 list-style-02 fw-500 text-center text-md-start">
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      </span>Hire a dedicated developer who works 8 hours a day, 5 days a week, committing 160 hours per month.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      </span>As your remote employee, they focus exclusively on your project, ensuring consistent progress.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      </span>This plan is perfect for projects with frequent changes or evolving requirements.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      Full-time developer costs start from $1000 to $2500, based on experience.
+                                                </li>
+                                          </ul>
+                                    </div>
+                              </div>
+                              <div class="pricing-footer ps-12 pe-12 pb-8 text-center">
+                                    <a href="#" class="btn btn-large btn-dark-gray btn-box-shadow btn-hover-animation-switch btn-round-edge w-100 text-transform-none mb-15px">
+                                          <span>
+                                                <span class="btn-text">Start Your Project </span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                          </span>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-lg-4 col-md-8"
+                        data-anime='{ "translateX": [-50, 0], "opacity": [0,1], "duration": 1200, "delay": 0, "staggervalue": 150, "easing": "easeOutQuad" }'>
+                        <div class="border-radius-6px position-relative overflow-hidden">
+                              <div
+                                    class="pricing-header pt-45px pb-10px border-radius-6px text-center position-relative top-minus-3px">
+                                    <span class="ps-25px pe-25px mb-15px text-uppercase text-base-color fs-13 lh-34 fw-700 border-radius-100px bg-solitude-blue d-inline-block">Enterprise</span>
+                                    <h5 class="fw-700 mb-0 text-dark-gray alt-font">On-Demand Hiring</h5>
+                                    <div class="pricing-body pt-35px">
+                                          <ul class="p-0 m-0 list-style-02 fw-500 text-center text-md-start">
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      Purchase a block of hours to hire a developer for smaller tasks over a flexible period.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      Once your hours are used up, simply buy more as needed, with no expiration date on the hours.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      This plan is great for on-demand work like small updates, bug fixes, or ongoing maintenance.
+                                                </li>
+                                                <li class="pt-15px pb-15px ps-12 pe-12 border-top border-color-extra-medium-gray text-dark-gray lg-ps-10 lg-pe-10">
+                                                      Hourly rates start from $10/hr, with blocks available in multiples of 24 hours.
+                                                </li>
+                                          </ul>
+                                    </div>
+                              </div>
+
+                              <div class="pricing-footer ps-12 pe-12 pb-8 text-center">
+                                    <a href="#"
+                                          class="btn btn-large btn-dark-gray btn-box-shadow btn-hover-animation-switch btn-round-edge w-100 text-transform-none mb-15px">
+                                          <span>
+                                                <span class="btn-text">Start Your Project </span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                                <span class="btn-icon"><i class="feather icon-feather-arrow-right"></i></span>
+                                          </span>
+                                    </a>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+            <div class="row row-cols-1 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 justify-content-center align-items-center">
+                  <!-- start features box item -->
+                  <div class="col icon-with-text-style-08 sm-mb-25px text-center text-sm-start md-mb-30px">
+                        <div class="d-flex justify-content-center">
+                              <div class="feature-box feature-box-left-icon-middle d-inline-flex align-middle">
+                                    <div class="feature-box-icon me-10px">
+                                          <i class="bi bi-rocket fs-20 text-dark-gray"></i>
+                                    </div>
+                                    <div class="feature-box-content">
+                                          <span class="fw-600 text-dark-gray">Launch Your Project in No Time</span>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+                  <!-- end features box item -->
+
+                  <!-- start features box item -->
+                  <div class="col icon-with-text-style-08 sm-mb-25px text-center text-sm-start md-mb-30px">
+                        <div class="d-flex justify-content-center">
+                              <div class="feature-box feature-box-left-icon-middle d-inline-flex align-middle">
+                                    <div class="feature-box-icon me-10px">
+                                          <i class="bi bi-shield-lock fs-20 text-dark-gray"></i>
+                                    </div>
+                                    <div class="feature-box-content">
+                                          <span class="fw-600 text-dark-gray">Secure and Reliable Solutions</span>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+                  <!-- end features box item -->
+
+                  <!-- start features box item -->
+                  <div class="col icon-with-text-style-08 text-center text-sm-start">
+                        <div class="d-flex justify-content-center">
+                              <div class="feature-box feature-box-left-icon-middle d-inline-flex align-middle">
+                                    <div class="feature-box-icon me-10px">
+                                          <i class="bi bi-gear fs-20 text-dark-gray"></i>
+                                    </div>
+                                    <div class="feature-box-content">
+                                          <span class="fw-600 text-dark-gray">Customizable to Your Needs</span>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+                  <!-- end features box item -->
+            </div>
+      </div>
+</section>
+
+
 <?php include_once(get_template_directory() . '/pages/homepage-logos.php'); ?>
 <?php include_once(get_template_directory() . '/global/contact-us-section.php'); ?>
 
